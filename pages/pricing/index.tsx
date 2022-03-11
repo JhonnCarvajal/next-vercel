75c41bb3d6883e608b3310b43b0323159ce979ff
JhonnyCarvajal <JhonnCarvajal@outlook.com>
2022-03-11 16:30:36 -0400

    Pre deployment in vercel

---
+++ pages/pricing/index.tsx
@@ -0,0 +1,19 @@
+import React from "react";
+import MainLayout from "../../components/layouts/MainLayout";
+import Link from "next/link";
+const pricing = () => {
+  return (
+    <MainLayout>
+      <h1>Pricing Page</h1>
+      <h1 className={"title"}>
+        Ir a <Link href="/">Home</Link>
+      </h1>
+
+      <p className={"description"}>
+        Get started by editing <code className={"code"}>pages/pricing.js</code>
+      </p>
+    </MainLayout>
+  );
+};
+
+export default pricing;
